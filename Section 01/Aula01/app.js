e8486a21b4c72b87d85119a49958e240f4751856
JoaoRobert0 <chavescamboim2004@gmail.com>
2024-01-11 14:24:31 -0300

    Primeiro Hello World

---
+++ Section 01/Aula01/app.js
@@ -0,0 +1,5 @@
+//Instalando o Node.js, VSCode, e Chrome no Windows
+
+// Para rodar o arquivo basta simplesmente clickar no "run" (devido ao "CodeRunner")
+// ou digitar "node [nome do arquivo.js]"
+console.log('Hello World');
